22e986a77274e2582d5952a13a8c00d41ab3a4ba
salehbot0 <saleh@cm.sa>
2026-03-07 02:45:28 +0300

    Initial code

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+            <h1>Next.js Provify Test</h1>
+            <p>This is a test Next.js application deployed by Provify.</p>
+        </div>
+    );
+}
